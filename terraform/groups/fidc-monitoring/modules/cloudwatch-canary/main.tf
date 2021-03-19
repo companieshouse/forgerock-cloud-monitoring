4dbca47f5a7ac2cc8f9208dc7eef3f302f19fc8b
Saul France <saul.france@amido.com>
2021-03-19 16:51:44 +0000

    fix: update parameters

--- terraform/groups/fidc-monitoring/modules/cloudwatch-canary/main.tf
+++ terraform/groups/fidc-monitoring/modules/cloudwatch-canary/main.tf
@@ -11,50 +11,82 @@ resource "aws_s3_bucket_object" "source_code" {
   etag   = data.archive_file.source_code.output_md5
 }
 
-# resource "aws_synthetics_canary" "canary" {
-#   name                 = var.canary_name
-#   artifact_s3_location = "s3://${var.artifact_bucket}/"
-#   execution_role_arn   = var.role_arn
-#   handler              = var.handler
-#   runtime_version      = var.runtime_version
-#   s3_bucket            = var.release_bucket
-#   s3_key               = "${var.service_name}/${var.environment}/${var.service_name}-${var.release_version}.zip"
-#   s3_version           = aws_s3_bucket_object.source_code.version_id
-
-#   schedule {
-#     expression = "rate(0 minute)"
-#   }
-# }
-
+# Using CloudFormation as canary environment variables
+# are not currently support by terraform
 resource "aws_cloudformation_stack" "canary" {
   name = "${var.service_name}-${var.canary_name}"
 
   parameters = {
-    VPCCidr = "10.0.0.0/16"
+    ARTIFACT_BUCKET          = "s3://${var.artifact_bucket}/"
+    HANDLER                  = var.handler
+    S3_BUCKET                = var.release_bucket
+    S3_KEY                   = "${var.service_name}/${var.environment}/${var.service_name}-${var.release_version}.zip"
+    S3_VERSION               = aws_s3_bucket_object.source_code.version_id
+    ROLE_ARN                 = var.role_arn
+    CANARY_NAME              = var.canary_name
+    FIDC_URL                 = var.fidc_url
+    FIDC_USER                = var.fidc_user
+    FIDC_PASSWORD            = var.fidc_password
+    FIDC_ADMIN_CLIENT        = var.fidc_admin_client
+    FIDC_ADMIN_CLIENT_SECRET = var.fidc_admin_client_secret
+    FIDC_CONNECTOR_GROUP     = var.fidc_connector_group
+    RUNTIME                  = var.runtime_version
+    HEALTH_CHECK_RATE        = var.rate_in_seconds
   }
 
   template_body = <<STACK
-Type: AWS::Synthetics::Canary
-Properties: 
-  ArtifactS3Location: "s3://${var.artifact_bucket}/"
-  Code: 
-    Handler: "${var.handler}"
-    S3Bucket: "${var.release_bucket}"
-    S3Key: "${var.service_name}/${var.environment}/${var.service_name}-${var.release_version}.zip"
-    S3ObjectVersion: "${aws_s3_bucket_object.source_code.version_id}"
-  ExecutionRoleArn: "${var.role_arn}"
-  Name: "${var.canary_name}"
-  RunConfig: 
-    EnvironmentVariables:
-      FIDC_URL: "${var.fidc_url}"
-      USER: "${var.fidc_user}"
-      PASSWORD: "${var.fidc_password}"
-      ADMIN_CLIENT: "${var.fidc_admin_client}"
-      ADMIN_CLIENT_SECRET: "${var.fidc_admin_client_secret}"
-      CONNECTOR_GROUP: "${var.fidc_connector_group}"
-  RuntimeVersion: "${var.runtime_version}"
-  Schedule: 
-    DurationInSeconds: "${var.rate_in_seconds}"
-  StartCanaryAfterCreation: true
+Parameters:
+  ARTIFACT_BUCKET:
+    Type: String
+  HANDLER:
+    Type: String
+  S3_BUCKET:
+    Type: String
+  S3_KEY:
+    Type: String
+  S3_VERSION:
+    Type: String
+  ROLE_ARN:
+    Type: String
+  CANARY_NAME:
+    Type: String
+  FIDC_URL:
+    Type: String
+  FIDC_USER:
+    Type: String
+  FIDC_PASSWORD:
+    Type: String
+  FIDC_ADMIN_CLIENT:
+    Type: String
+  FIDC_ADMIN_CLIENT_SECRET:
+    Type: String
+  FIDC_CONNECTOR_GROUP:
+    Type: String
+  HEALTH_CHECK_RATE:
+    Type: String
+Resources
+  canary:
+    Type: AWS::Synthetics::Canary
+    Properties: 
+      ArtifactS3Location: !Ref ARTIFACT_BUCKET
+      Code: 
+        Handler: !Ref HANDLER
+        S3Bucket: !Ref S3_BUCKET
+        S3Key: !Ref S3_KEY
+        S3ObjectVersion: !Ref S3_VERSION
+      ExecutionRoleArn: !Ref ROLE_ARN
+      Name: !Ref CANARY_NAME
+      RunConfig: 
+        EnvironmentVariables:
+          FIDC_URL: !Ref FIDC_URL
+          USER: !Ref FIDC_USER
+          PASSWORD: !Ref FIDC_PASSWORD
+          ADMIN_CLIENT: !Ref FIDC_ADMIN_CLIENT
+          ADMIN_CLIENT_SECRET: !Ref FIDC_ADMIN_CLIENT_SECRET
+          CONNECTOR_GROUP: !Ref FIDC_CONNECTOR_GROUP
+      RuntimeVersion: !Ref RUNTIME
+      Schedule: 
+        DurationInSeconds: !Ref HEALTH_CHECK_RATE
+      StartCanaryAfterCreation: true
 STACK
 }
