19d4d6a40a3992b1d8a8622cc9261a3c84b7eb1c
Saul France <saul.france@amido.com>
2021-03-19 11:56:40 +0000

    chore: add s3 bucket and role for canaries

--- terraform/groups/fidc-monitoring/main.tf
+++ terraform/groups/fidc-monitoring/main.tf
@@ -20,6 +20,8 @@ data "aws_subnet_ids" "subnets" {
 ###
 module "cloudwatch" {
   source            = "./modules/cloudwatch"
+  region            = var.region
+  environment       = var.environment
   service_name      = var.service_name
   retention_in_days = var.log_retention_in_days
 }
--- terraform/groups/fidc-monitoring/modules/cloudwatch/main.tf
+++ terraform/groups/fidc-monitoring/modules/cloudwatch/main.tf
@@ -1,4 +1,70 @@
 resource "aws_cloudwatch_log_group" "monitoring" {
-  name = var.service_name
+  name              = var.service_name
   retention_in_days = var.retention_in_days
+}
+
+resource "aws_s3_bucket" "canary_artifacts" {
+  bucket        = "${var.environment}-${var.region}.${var.service_name}.ch.gov.uk"
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "canary_role" {
+  version = "2012-10-17"
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "canary_role" {
+  name               = "${var.service_name}-canary"
+  assume_role_policy = data.aws_iam_policy_document.canary_role.json
+}
+
+resource "aws_iam_policy" "canary_role" {
+  name = "${var.service_name}-canary"
+  path = "/"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.canary_artifacts.arn}/*"
+      },
+      {
+        Action = [
+          "s3:GetBucketLocation",
+        ]
+        Effect   = "Allow"
+        Resource = aws_s3_bucket.canary_artifacts.arn
+      },
+      {
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:CreateLogGroup",
+          "s3:ListAllMyBuckets",
+          "cloudwatch:PutMetricData",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "canary_role" {
+  role       = aws_iam_role.canary_role.name
+  policy_arn = aws_iam_policy.canary_role.arn
 }
