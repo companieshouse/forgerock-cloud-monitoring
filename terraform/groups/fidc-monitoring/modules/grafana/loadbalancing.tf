b51c66478023f84ae474dd218c3daafd5f57534a
Saul France <saul.france@amido.com>
2021-05-04 15:45:52 +0100

    chore: add lb health checks

--- terraform/groups/fidc-monitoring/modules/grafana/loadbalancing.tf
+++ terraform/groups/fidc-monitoring/modules/grafana/loadbalancing.tf
@@ -12,6 +12,14 @@ resource "aws_lb_target_group" "grafana" {
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    path                = "/login"
+    interval            = 60
+  }
 }
 
 resource "aws_lb_target_group_attachment" "grafana" {
