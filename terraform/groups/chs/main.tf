4483a40bb4da9d9ff51e50c1f97048823e625f0c
Darren Smallwood <dsmallwood@companieshouse.gov.uk>
2022-03-17 09:17:51 +0000

    added canary for webfilingauthcode

--- terraform/groups/chs/main.tf
+++ terraform/groups/chs/main.tf
@@ -166,6 +166,32 @@ module "connectors_chscompany" {
   tags                     = local.common_tags
 }
 
+module "connectors_WfAuthCode" {
+  source                   = "./modules/cloudwatch-canary"
+  region                   = var.region
+  environment              = var.environment
+  service_name             = var.service_name
+  canary_name              = "connector-WfAuthCode"
+  release_version          = var.container_image_version
+  source_code_path         = "${path.module}/scripts/mappings-monitoring"
+  handler                  = "index.handler"
+  runtime_version          = "syn-nodejs-puppeteer-3.3"
+  release_bucket           = var.release_bucket
+  artifact_bucket          = module.cloudwatch.canary_artifact_bucket
+  role_arn                 = module.cloudwatch.canary_role_arn
+  health_check_rate        = var.health_check_rate
+  fidc_url                 = var.fidc_url
+  fidc_user                = var.fidc_user
+  fidc_password            = var.fidc_password
+  fidc_admin_client        = var.fidc_admin_client
+  fidc_admin_client_secret = var.fidc_admin_client_secret
+  fidc_monitored_component = "WebfilingAuthCode"
+  recon_duration           = var.recon_duration
+  cancel_recon_after       = var.cancel_recon_after
+  sns_topic_arn            = module.alerting.sns_topic_arn
+  tags                     = local.common_tags
+}
+
 module "reconDuration" {
   source                   = "./modules/cloudwatch-canary"
   region                   = var.region
