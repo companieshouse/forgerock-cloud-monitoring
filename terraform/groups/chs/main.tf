d7c41daf23a2bced633ba89f72c8f05b546dbab3
Darren Smallwood <dsmallwood@companieshouse.gov.uk>
2022-03-23 14:56:25 +0000

    added mapping canaries to tf

--- terraform/groups/chs/main.tf
+++ terraform/groups/chs/main.tf
@@ -88,12 +88,12 @@ module "rcs_monitoring" {
   tags                     = local.common_tags
 }
 
-module "mappings_monitoring" {
+module "mapping_chscompany" {
   source                   = "./modules/cloudwatch-canary"
   region                   = var.region
   environment              = var.environment
   service_name             = var.service_name
-  canary_name              = "fr-mappings"
+  canary_name              = "mapping-chscompany"
   release_version          = var.container_image_version
   source_code_path         = "${path.module}/scripts/mappings-monitoring"
   handler                  = "index.handler"
@@ -107,7 +107,59 @@ module "mappings_monitoring" {
   fidc_password            = var.fidc_password
   fidc_admin_client        = var.fidc_admin_client
   fidc_admin_client_secret = var.fidc_admin_client_secret
-  fidc_monitored_component = var.fidc_mappings
+  fidc_monitored_component = "chsMongoCompanyProfile_alphaOrg"
+  recon_duration           = var.recon_duration
+  cancel_recon_after       = var.cancel_recon_after
+  sns_topic_arn            = module.alerting.sns_topic_arn
+  tags                     = local.common_tags
+}
+
+module "mapping_wfauthcode" {
+  source                   = "./modules/cloudwatch-canary"
+  region                   = var.region
+  environment              = var.environment
+  service_name             = var.service_name
+  canary_name              = "mapping-wfauthcode"
+  release_version          = var.container_image_version
+  source_code_path         = "${path.module}/scripts/mappings-monitoring"
+  handler                  = "index.handler"
+  runtime_version          = "syn-nodejs-puppeteer-3.4"
+  release_bucket           = var.release_bucket
+  artifact_bucket          = module.cloudwatch.canary_artifact_bucket
+  role_arn                 = module.cloudwatch.canary_role_arn
+  health_check_rate        = var.health_check_rate
+  fidc_url                 = var.fidc_url
+  fidc_user                = var.fidc_user
+  fidc_password            = var.fidc_password
+  fidc_admin_client        = var.fidc_admin_client
+  fidc_admin_client_secret = var.fidc_admin_client_secret
+  fidc_monitored_component = "webfilingAuthCode_alphaOrg"
+  recon_duration           = var.recon_duration
+  cancel_recon_after       = var.cancel_recon_after
+  sns_topic_arn            = module.alerting.sns_topic_arn
+  tags                     = local.common_tags
+}
+
+module "mapping_wfuser" {
+  source                   = "./modules/cloudwatch-canary"
+  region                   = var.region
+  environment              = var.environment
+  service_name             = var.service_name
+  canary_name              = "mapping-wfuser"
+  release_version          = var.container_image_version
+  source_code_path         = "${path.module}/scripts/mappings-monitoring"
+  handler                  = "index.handler"
+  runtime_version          = "syn-nodejs-puppeteer-3.4"
+  release_bucket           = var.release_bucket
+  artifact_bucket          = module.cloudwatch.canary_artifact_bucket
+  role_arn                 = module.cloudwatch.canary_role_arn
+  health_check_rate        = var.health_check_rate
+  fidc_url                 = var.fidc_url
+  fidc_user                = var.fidc_user
+  fidc_password            = var.fidc_password
+  fidc_admin_client        = var.fidc_admin_client
+  fidc_admin_client_secret = var.fidc_admin_client_secret
+  fidc_monitored_component = "webfilingUser_alphaUser"
   recon_duration           = var.recon_duration
   cancel_recon_after       = var.cancel_recon_after
   sns_topic_arn            = module.alerting.sns_topic_arn
@@ -192,12 +244,12 @@ module "connector_wfuser" {
   tags                     = local.common_tags
 }
 
-module "MongoCompanyProfile_Duration" {
+module "duration_chscompany" {
   source                   = "./modules/cloudwatch-canary"
   region                   = var.region
   environment              = var.environment
   service_name             = var.service_name
-  canary_name              = "mongocompprofile-dur"
+  canary_name              = "duration-chscompany"
   release_version          = var.container_image_version
   source_code_path         = "${path.module}/scripts/mappings-monitoring"
   handler                  = "index.handler"
@@ -218,12 +270,12 @@ module "MongoCompanyProfile_Duration" {
   tags                     = local.common_tags
 }
 
-module "webfilingAuthCode_Duration" {
+module "duration_wfauthcode" {
   source                   = "./modules/cloudwatch-canary"
   region                   = var.region
   environment              = var.environment
   service_name             = var.service_name
-  canary_name              = "webfilingauthcode-dur"
+  canary_name              = "duration-wfauthcode"
   release_version          = var.container_image_version
   source_code_path         = "${path.module}/scripts/mappings-monitoring"
   handler                  = "index.handler"
@@ -244,12 +296,12 @@ module "webfilingAuthCode_Duration" {
   tags                     = local.common_tags
 }
 
-module "webfilingUser_Duration" {
+module "duration_wfuser" {
   source                   = "./modules/cloudwatch-canary"
   region                   = var.region
   environment              = var.environment
   service_name             = var.service_name
-  canary_name              = "webfilinguser-dur"
+  canary_name              = "duration-wfuser"
   release_version          = var.container_image_version
   source_code_path         = "${path.module}/scripts/mappings-monitoring"
   handler                  = "index.handler"
