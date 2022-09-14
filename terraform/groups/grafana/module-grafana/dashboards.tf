9030c3a5a17e0afce4e4e0d4262a32fca744109b
Richard Coupe <richard.coupe@companieshouse.gov.uk>
2022-09-14 12:02:09 +0100

    IDAM-1416: Add explanatory comment

--- terraform/groups/grafana/module-grafana/dashboards.tf
+++ terraform/groups/grafana/module-grafana/dashboards.tf
@@ -3,6 +3,7 @@ provider "grafana" {
     auth = "${var.grafana_service_user}:${var.grafana_service_password}"
 }
 
+// Create a dashboard for each json file found in the dashboards directory
 resource "grafana_dashboard" "dashboard" {
     for_each = fileset(path.module, "dashboards/*.json")
     config_json = file("${path.module}/${each.key}")
