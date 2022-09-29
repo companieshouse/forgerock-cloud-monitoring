2609740259381d268809f38c6b70dc88316d8ed3
Richard Coupe <richard.coupe@companieshouse.gov.uk>
2022-09-29 13:43:05 +0100

    IDAM-1418: Add lambda to restart ECS services

---
+++ terraform/groups/chs/modules/ecs-restart-lambda/lambda_code/index.js
@@ -0,0 +1,12 @@
+const AWS = require('aws-sdk');
+
+exports.main = async function(event, context) {
+    const ecs = new AWS.ECS();
+    ecs.UpdateService({
+        cluster: process.env.ecs_cluster_arn,
+        service: process.env.ecs_cluster_name,
+        forceNewDeployment: true
+    }).catch(error => {
+            throw error;
+        });
+}
---
+++ terraform/groups/chs/modules/ecs-restart-lambda/variables.tf
@@ -0,0 +1,15 @@
+variable "ecs_cluster_arn" {
+    type = string
+}
+
+variable "ecs_service_arn" {
+    type = string
+}
+
+variable "ecs_service_name" {
+    type = string
+}
+
+variable "restart_frequency_schedule" {
+    type = string
+}
