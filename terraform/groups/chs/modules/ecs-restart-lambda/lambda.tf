30b877c7af229358355f99acd5399d4b7e1ecb31
Richard Coupe <richard.coupe@companieshouse.gov.uk>
2022-09-30 16:10:36 +0100

    IDAM-1418: Just use the terraform method of zipping the code bundle ...

--- terraform/groups/chs/modules/ecs-restart-lambda/lambda.tf
+++ terraform/groups/chs/modules/ecs-restart-lambda/lambda.tf
@@ -1,10 +1,20 @@
+locals {
+    zip_file_path = "${path.module}/ecs_service_restart.zip"
+}
+
+data "archive_file" "lambda_code" {
+    type = "zip"
+    source_dir = "lambda_code"
+    output_path = local.zip_file_path
+}
+
 resource "aws_lambda_function" "restart_function" {
-    filename = "ecs_service_restart.zip"
+    filename = local.zip_file_path
     function_name = "ecs_service_restart_${var.ecs_service_name}"
     role = aws_iam_role.execution_role
     handler = "index.main"
 
-    source_code_hash = filebase64sha256("ecs_service_restart.zip")
+    source_code_hash = filebase64sha256(local.zip_file_path)
     runtime = "nodejs12.x"
 
     environment {
@@ -13,4 +23,8 @@ resource "aws_lambda_function" "restart_function" {
             ecs_service_name = var.ecs_service_name
         }
     }
+
+    depends_on = [
+        data.archive_file.lambda_code
+    ]
 }
